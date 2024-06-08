f13c2d9a4c40009bf16062564e03cbc80ee147ee
Joseph C. Caswell <josephcaswell@tpm.com>
2024-06-07 21:33:40 -0400

    Scripts in Head
    Moved the scripts to a more standard location: in the head tag. Previously they were at the end of the body.

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // COPY THIS FILE TO 'configUser.js' AND UPDATE THE VALUES TO MATCH YOUR SERVER URL & GROUP ALIAS
-// Version 1.2.7
+// Version 1.2.8
 
 const config = {
-	version: "1.2.7",
+	version: "1.2.8",
 	// the full path the site running the DriveWorks Live API
 	// serverUrl: "https://dw21.api.yourdomain.com",
 	serverUrl: "",
