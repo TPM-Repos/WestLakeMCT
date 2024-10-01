c33786835a7f74fc4943f839e5abe9cb5f601c62
Joseph C. Caswell <josephcaswell@tpm.com>
2024-10-01 14:56:41 -0400

    Theming

---
+++ configUser.js
@@ -0,0 +1,205 @@
+const config = {
+version: "1.2.9",
+	// the full path the site running the DriveWorks Live API
+	// serverUrl: "https://dw21.api.yourdomain.com",
+	serverUrl: "https://dw21.api.tpmautomation.com",
+	// The default alias for the DriveWorks Group
+	// This is a custom string that must match the name in the ConfigUser.xml file
+	groupAlias: "Dover",
+	// (Optional) Configure ping & update intervals - in seconds
+    // (Optional) Set Specification ping interval - in seconds
+    // A Specification will timeout after a configured period of inactivity (see DriveWorksConfigUser.xml).
+    // This function prevents a Specification timing out as long as the page is in view.
+    // Disable the ping by setting to 0
+    specificationPingInterval: 30,
+	// (Optional) Enter custom redirect URLs for login/logout and Project/DriveApp close/cancel
+	folder: "",
+	login: {
+		redirectUrl: "projects.html",
+		// set this if you want to redirect guest users to a different page
+		redirectGuestUrl: "projects.html",
+		// Set this to left, center, or right to position the login form on the page
+		columnLocation: "left",
+	},
+	logout: {
+		redirectUrl: "index.html",
+	},
+	project: {
+		// you may put "logout" instead of a page location
+		redirectOnClose: "details.html",
+		redirectOnCancel: "projects.html",
+	},
+	driveApp: {
+		// you may put "logout" instead of a page location
+		redirectOnClose: "details.html",
+		redirectOnCancel: "drive-apps.html",
+	},
+	// (Optional) Configure 'Run' view
+	run: {
+		showWarningOnExit: true, // Toggle warning dialog when exiting "Run" view with potentially unsaved changes (where supported)
+		loadCustomProjectAssets: {
+			scripts: false,
+			styles: false,
+		},
+	},
+	// (Optional) Configure 'Details' view
+	details: {
+		updateInterval: 5, // Interval to refresh content - in seconds
+		showStartNewSpecificationAction: true,
+	},
+	// (Optional) Configure the query function
+	// Enter a default Group Alias and/or Project name to be used (when none are passed in the query string)
+	// Choose how sessions are handled
+	query: {
+		defaultGroupAlias: "",
+		defaultProjectName: "",
+		autoLogin: false,
+		requireNewSession: true,
+		requireExactAlias: false,
+	},
+	copyright: {
+		show: true,
+		holder: "WestLake Corporation",
+		year: "2024",
+	},
+	// Add a watermark over pages in order to indicate that the site is a development site
+	// comment out or set to "" to disable
+	watermark: "Development",
+	// Set the title of the site, this will be displayed in the browser tab
+	// pageName | siteName
+	siteName: "WestLake Corporation",
+	// Set whether a username or email address will be used
+	// username | email address
+	usernameType: "Username",
+	loginReturnUrls: true, // Toggle appending return urls to restore the previous location when redirected to the login form
+	locale: "en-US", // Set the default locale for displaying dates and numbers
+	dateFormat: {
+		month: "long",
+		day: "numeric",
+		year: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	},
+	// Whether to show debugging information in the console
+	debug: false,
+	allowSingleSignOn: false,
+	guestLogin: {
+		enabled: true,
+		alias: "Westlake Guest",
+	},
+	accountManagement: {
+		createAccount: "query?alias=TemplatesGuest&run=AccountManagement&DWMacroNavigate=CreateAccount",
+		forgotPassword: "query?alias=TemplatesGuest&run=AccountManagement&DWMacroNavigate=ForgotPassword",
+		resetPassword:
+			"query?alias=Templates&run=AccountManagement&DWMacroNavigate=ResetPassword",
+	},
+	sidebarLinks: [
+		{
+			title: "Projects",
+			icon: "projects",
+			href: "projects.html",
+		},
+		// {
+		// 	title: "DriveApps",
+		// 	icon: "drive-apps",
+		// 	href: "drive-apps.html",
+		// },
+		{
+			title: "History",
+			icon: "history",
+			href: "history.html",
+		},
+	],
+	images: {
+		// You may use a different (or same) company logo for the login and sidebar
+		// You may wish to do this due to the color of the logo and contrast with the background color
+		// Here is an example with svgs and with pngs
+		// login: "dist/img/logo-dark.svg",
+		// sidebar: "dist/img/logo-light.svg",
+		login: "dist/img/logo.png",
+		sidebar: "dist/img/logo.png",
+		// By default the login screen will show a static cover image
+		// You can change it to a different image here.
+		loginCover: "dist/img/login-cover.png",
+		// You can use a series of images instead of a static cover image by enabling the carousel
+		// These will fade into the next image every 'interval' seconds
+		// You may use as many as you want but the more you use the longer the page will take to load
+		carousel: {
+			enabled: true,
+			interval: 5,
+			images: [
+				"dist/img/slide1.webp",
+				"dist/img/slide2.webp",
+				"dist/img/slide3.webp",
+				"dist/img/slide4.webp",
+				"dist/img/slide5.webp",
+				"dist/img/slide6.webp",
+				"dist/img/slide7.webp",
+				"dist/img/slide8.webp",
+				"dist/img/slide10.webp",
+			],
+		},
+	},
+	// Use this section to easily set the branding of your site.
+	// Available fonts are Roboto (Flex), Inter, and Poppins
+	// you may @import any additional fonts you require in dist/css/theme/theme.css: for more info: https://www.w3schools.com/css/css3_fonts.asp
+	// Sizes can be various units (%, pt, px, em, rem, vh, vw, etc): for more info: https://www.w3schools.com/css/css_units.asp
+	// don't use % with radius
+	// Colors can be names, hexadecimal, rgb(a), hsl(a): for more info: https://www.w3schools.com/cssref/css_colors_legal.php
+	// line height can be px, pt, or unit-less. unit-less is * font size
+	styles: {
+        text: {
+            font: "Roboto",
+            size: "14px",
+            color: "black",
+            lineHeight: "20px",
+        },
+        heading: {
+            font: "RobotoSlab-Regular, Roboto, sans-serif",
+            size: "36px",
+            color: "#3b5795",
+            weight: "500",
+            lineHeight: "40px",
+        },
+        caption: {
+            font: "Roboto",
+            size: "12pt",
+            color: "black",
+            weight: "light",
+        },
+        color: {
+            primary: "#000000",
+            secondary: "#3b5795",
+            background: "#edeeee",
+            icon: "black",
+            focus: "#00AEEF",
+        },
+        sidebar: {
+            background: "white",
+            width: "18em",
+            logoPadding: "1em",
+			textColor: "black",
+        },
+        loginForm: {
+            background: "white",
+            padding: "1em",
+			textColor: "black",
+        },
+        button: {
+			// not recommended to use %
+            radius: "0",
+            color: "#3b5795",
+        },
+        logo: {
+            width: "50%"
+        },
+        projectCard: {
+            background: "white",
+            margin: "10px",
+        },
+		// not recommended to use %
+        inputRadius: "0",
+    },
+}
+
