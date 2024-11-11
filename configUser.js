5ee09bcf00d30be06429dad6dcf1255b865e6f30
Joseph C. Caswell <josephcaswell@tpm.com>
2024-11-10 21:57:04 -0500

    New Session not required
    Fixes #9

--- configUser.js
+++ configUser.js
@@ -59,7 +59,7 @@ const config = {
 		defaultGroupAlias: "",
 		defaultProjectName: "",
 		autoLogin: false,
-		requireNewSession: true,
+		requireNewSession: false,
 		requireExactAlias: false,
 	},
 	copyright: {
